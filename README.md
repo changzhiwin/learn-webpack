ab6b14bea7ef820061fb86032157360cd15d8536
changzhiwin <changzhiwin@gmail.com>
2019-05-11 10:36:18 +0800

    asset-management, images

---
+++ README.md
@@ -0,0 +1,2 @@
+# learn-webpack
+学习[教程](https://webpack.js.org/guides)，把每个步骤打一个tag，方便回溯
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: 'images'
+            }
+          }
+        ],
+      }
+    ]
+  }
+};
